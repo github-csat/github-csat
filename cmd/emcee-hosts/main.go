c78bd0ce678aab1c91b37ba124131d5e09b37121
dexhorthy <dexter@replicated.com>
2022-11-04 19:58:07 -0500

    lets put it in the cluster

--- cmd/emcee-hosts/main.go
+++ cmd/emcee-hosts/main.go
@@ -8,13 +8,11 @@ import (
 func main() {
 	router := gin.Default()
 
-	// Query string parameters are parsed using the existing underlying request object.
-	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
-	router.GET("/welcome", func(c *gin.Context) {
-		firstname := c.DefaultQuery("firstname", "Guest")
-		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+	router.GET("/submit", func(c *gin.Context) {
+		issueURL := c.Query("issue")
+		responseValue := c.Query("response")
 
-		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+		c.String(http.StatusOK, "Your response on %s has been recorded as %s", issueURL, responseValue)
 	})
 	router.Run(":8080")
 }
