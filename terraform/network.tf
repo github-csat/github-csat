1fb668590fcf46e57f070447580ad99417d91930
dexhorthy <dexter@replicated.com>
2022-12-27 18:43:47 -0600

    its ingress o clock fellas

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,5 +1,10 @@
 # soon - create a static IP for the ingress
-#resource "google_compute_address" "default" {
-#  name   = "my-test-static-ip-address"
-#  region = "us-central1"
-#}
+resource "google_compute_address" "prod-ingress" {
+  name   = "my-test-static-ip-address"
+  region = "us-central1"
+}
+
+
+output "static-ip" {
+  data = google_compute_address.prod-ingress.address
+}
