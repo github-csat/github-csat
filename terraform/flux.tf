ba85329961c38bb4067e57061f23443e13c0fdd1
dexhorthy <dexter@replicated.com>
2023-02-02 21:30:48 -0600

    wow what

--- terraform/flux.tf
+++ terraform/flux.tf
@@ -9,10 +9,10 @@ module "gke_auth" {
   use_private_endpoint = false
 
   # don't do anything until the cluster is ready
-  depends_on = [
-    google_container_cluster.prod,
-    google_container_node_pool.primary_preemptible_nodes,
-  ]
+#  depends_on = [
+#    google_container_cluster.prod,
+#    google_container_node_pool.primary_preemptible_nodes,
+#  ]
 }
 
 provider "kubernetes" {
@@ -36,85 +36,85 @@ data "flux_install" "main" {
 }
 
 # create namespace
-#resource "kubernetes_namespace" "flux_system" {
-#  metadata {
-#    name = "flux-system"
-#  }
-#
-#  lifecycle {
-#    ignore_changes = [
-#      metadata[0].labels,
-#    ]
-#  }
-#  depends_on = [
-#    google_container_cluster.prod,
-#    google_container_node_pool.primary_preemptible_nodes,
-#  ]
-#}
-#
-## Split multi-doc YAML with
-## https://registry.terraform.io/providers/gavinbunney/kubectl/latest
-#data "kubectl_file_documents" "apply" {
-#  content = data.flux_install.main.content
-#}
-#
-## Convert documents list to include parsed yaml data
-#locals {
-#  apply = [for v in data.kubectl_file_documents.apply.documents : {
-#    data : yamldecode(v)
-#    content : v
-#    }
-#  ]
-#}
-#
-#### Sync
-## Apply manifests on the cluster
-#resource "kubectl_manifest" "apply" {
-#  for_each   = { for v in local.apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-#  depends_on = [kubernetes_namespace.flux_system]
-#  yaml_body  = each.value
-#}
-#
-#
-## Generate manifests
-#data "flux_sync" "main" {
-#  target_path = "kustomize/overlays/prod"
-#  url         = "https://github.com/github-csat/github-csat"
-#}
-#
-## Split multi-doc YAML with
-## https://registry.terraform.io/providers/gavinbunney/kubectl/latest
-#data "kubectl_file_documents" "sync" {
-#  content = data.flux_sync.main.content
-#}
-#
-## Convert documents list to include parsed yaml data
-#locals {
-#  sync = [for v in data.kubectl_file_documents.sync.documents : {
-#    data : yamldecode(v)
-#    content : v
-#    }
-#  ]
-#}
-#
-## Apply manifests on the cluster
-#resource "kubectl_manifest" "sync" {
-#  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
-#  depends_on = [kubernetes_namespace.flux_system]
-#  yaml_body  = each.value
-#}
-#
-## Generate a Kubernetes secret with the Git credentials
-#resource "kubernetes_secret" "main" {
-#  depends_on = [kubectl_manifest.apply]
-#
-#  metadata {
-#    name      = data.flux_sync.main.secret
-#    namespace = data.flux_sync.main.namespace
-#  }
-#
-#  data = {
-#    username = "git"
-#    password = var.flux_token
-#  }
-#}
+resource "kubernetes_namespace" "flux_system" {
+  metadata {
+    name = "flux-system"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metadata[0].labels,
+    ]
+  }
+  depends_on = [
+    google_container_cluster.prod,
+    google_container_node_pool.primary_preemptible_nodes,
+  ]
+}
+
+# Split multi-doc YAML with
+# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
+data "kubectl_file_documents" "apply" {
+  content = data.flux_install.main.content
+}
+
+# Convert documents list to include parsed yaml data
+locals {
+  apply = [for v in data.kubectl_file_documents.apply.documents : {
+    data : yamldecode(v)
+    content : v
+    }
+  ]
+}
+
+### Sync
+# Apply manifests on the cluster
+resource "kubectl_manifest" "apply" {
+  for_each   = { for v in local.apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  depends_on = [kubernetes_namespace.flux_system]
+  yaml_body  = each.value
+}
+
+
+# Generate manifests
+data "flux_sync" "main" {
+  target_path = "kustomize/overlays/prod"
+  url         = "https://github.com/github-csat/github-csat"
+}
+
+# Split multi-doc YAML with
+# https://registry.terraform.io/providers/gavinbunney/kubectl/latest
+data "kubectl_file_documents" "sync" {
+  content = data.flux_sync.main.content
+}
+
+# Convert documents list to include parsed yaml data
+locals {
+  sync = [for v in data.kubectl_file_documents.sync.documents : {
+    data : yamldecode(v)
+    content : v
+    }
+  ]
+}
+
+# Apply manifests on the cluster
+resource "kubectl_manifest" "sync" {
+  for_each   = { for v in local.sync : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content }
+  depends_on = [kubernetes_namespace.flux_system]
+  yaml_body  = each.value
+}
+
+# Generate a Kubernetes secret with the Git credentials
+resource "kubernetes_secret" "main" {
+  depends_on = [kubectl_manifest.apply]
+
+  metadata {
+    name      = data.flux_sync.main.secret
+    namespace = data.flux_sync.main.namespace
+  }
+
+  data = {
+    username = "git"
+    password = var.flux_token
+  }
+}
